b6de4e5006b4b5d4cdb362a26a262726bfab44e0
MACRODAT <macrodatservices@gmail.com>
2020-10-12 03:18:51 +0000

    Created App component

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// entry point
+import App from './app';
+    
+ReactDOM.render(
+    <App />
+    , 
+    document.querySelector('#root'))
