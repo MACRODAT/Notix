7cb3d9c21727521c9d93032222d3b92fd8cb49a4
MACRODAT <macrodatservices@gmail.com>
2020-10-20 19:07:41 +0000

    removed sensitive data

---
+++ Blockchain/client.ignore.tsx
@@ -0,0 +1,10 @@
+import 'factom-harmony-connect';
+
+// Configure API key authorization
+
+let token = {
+    'appId' : '2814cb97',
+    'appKey' : 'fe5beda3e4693668e02919f5e9ee5d82', // TODO : ADD TO GITIGNORE
+}
+
+
